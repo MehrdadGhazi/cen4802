70630b125e1d50e63ae6cf2e1fc154990a4a7df7
MehrdadGhazi <105956706+MehrdadGhazi@users.noreply.github.com>
2022-09-04 14:12:45 -0400

    Update Ghazi_Fibonacci.java

--- Ghazi_Fibonacci.java
+++ Ghazi_Fibonacci.java
@@ -11,7 +11,12 @@ public class Ghazi_Fibonacci {
 		System.out.println("The " + passedNum + "th number of the Fibonacci sequence is " + fibonacciRecursive(10));
 		
 	}
-	
+	/**
+	 * The Fibonacci methods uses recursion to return the nth number in the Fibonacci sequence
+	 * @author Mehrdad Ghazi
+	 * @param n the number in the sequence you want to calculate
+	 * @return returns the nth number in the Fibonacci sequence
+	 */
 	public static int fibonacciRecursive(int n) {
 		if(n <= 1) {
 			return n;
